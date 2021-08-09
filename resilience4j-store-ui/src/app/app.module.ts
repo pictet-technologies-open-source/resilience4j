607d14fe64ccd45a8f067636870a6ff3a247cc01
Alexandre JACQUOT <77357704+alexandre-jacquot-ptl@users.noreply.github.com>
2021-08-09 22:39:10 +0200

    Update app.module.ts

--- resilience4j-store-ui/src/app/app.module.ts
+++ resilience4j-store-ui/src/app/app.module.ts
@@ -6,17 +6,17 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input'
 import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatSelectModule} from "@angular/material/select";
-import {MatIconModule} from "@angular/material/icon";
-import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {ItemListComponent} from './component/item-list/item-list.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {HttpErrorsInterceptor} from "./interceptor/http-errors-interceptor";
-import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {HttpErrorsInterceptor} from './interceptor/http-errors-interceptor';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {ItemComponent} from './component/item/item.component';
-import {MatCardModule} from "@angular/material/card";
-import {MatGridListModule} from "@angular/material/grid-list";
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @NgModule({
     declarations: [

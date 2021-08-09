55d50410ac75e7ece837d0adca61eba26ca0c7bb
Alexandre JACQUOT <77357704+alexandre-jacquot-ptl@users.noreply.github.com>
2021-08-09 22:42:36 +0200

    Update app.module.ts

--- resilience4j-store-ui/src/app/app.module.ts
+++ resilience4j-store-ui/src/app/app.module.ts
@@ -4,8 +4,8 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input'
-import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';

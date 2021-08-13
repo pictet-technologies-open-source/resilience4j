838c51cec414a646d8319cfd5da8f68ca5ebe376
Alexandre Jacquot <ajacquot@pictet-technologies.com>
2021-08-13 18:05:22 +0200

    Retry

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # resilience4j
 Resilience4j showcase
+
+## Compile the application
+
+```
+$ mvn clean install
+```
+
+## Start the application
+
+### Prometheus and Grafana
+
+```
+$ cd docker
+$ docker-compose up
+```
+
+In Grafana you will have to create a new dashboard and import the file docker/grafana.json
+
+
+### Start the exchange rate webservice
+
+```
+$ cd resilience4j-exchange-rate-proxy
+$ mvn spring-boot:run
+```
+
+### Start the resilient store webservice
+
+```
+$ cd resilience4j-store-app
+$ mvn spring-boot:run
+```
+
+### Start the UI
+
+```
+$ cd resilience4j-store-ui
+$ npm start
+```
+
+## URLs
+
+Web application : http://localhost:4200/#/
+Actuator : http://localhost:8080/actuator/
+Prometheus : http://localhost:9090/classic/targets#job-store-app
+Grafana : http://localhost:3000/?orgId=1
+Swagger : http://localhost:8080/swagger-ui/#/currency-controller/getExchangeRatesUsingGET
+
+## Call the application using Apache HTTP Server bechnmarking tool
+
+```
+ab -n x -c y 'http://127.0.0.1:4200/items?currency=GBP'
+```
+
+where x is the number of call
+and y the number of concurrent users  
--- resilience4j-store-app/src/main/java/com/pictet/technologies/resilience/resilience4j/store/service/ExchangeRateService.java
+++ resilience4j-store-app/src/main/java/com/pictet/technologies/resilience/resilience4j/store/service/ExchangeRateService.java
@@ -4,6 +4,7 @@ import com.pictet.technologies.resilience.resilience4j.store.provider.exchangera
 import com.pictet.technologies.resilience.resilience4j.store.provider.exchangerate.api.CurrencyExchangeRates;
 import feign.FeignException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,28 +17,55 @@ import java.math.RoundingMode;
 @Slf4j
 public class ExchangeRateService {
 
+    private static final String RESILIENCE_NAME = "exchangeRateService-get";
+
     private final ExchangeRateClient exchangeRateClient;
 
     // Cached exchange rates, for recovery purpose
     private CurrencyExchangeRates latestExchangesRates;
 
-    @CircuitBreaker(name = "getExchangeRates", fallbackMethod = "getExchangeRatesFallback")
+    // When using retry and circuit breaker together
+    // - Don't specify a fallback if you are also using a circuit breaker
+    // - Change the aspect order (see the application.yaml)
+    @Retry(name = ExchangeRateService.RESILIENCE_NAME /*, fallbackMethod = "getExchangeRatesRetryFallback"*/ )
+    @CircuitBreaker(name = ExchangeRateService.RESILIENCE_NAME, fallbackMethod = "getExchangeRatesCircuitBreakerFallback")
     public CurrencyExchangeRates getExchangeRates(String currency) {
-        final CurrencyExchangeRates rates = exchangeRateClient.getLatest(currency, BigDecimal.ONE);
-        this.latestExchangesRates = rates;
-        return rates;
+
+        try {
+            log.info("Call the exchange rate service");
+
+            final CurrencyExchangeRates rates = exchangeRateClient.getLatest(currency, BigDecimal.ONE);
+            this.latestExchangesRates = rates;
+            return rates;
+        } catch(Exception ex) {
+            log.error("getExchangeRates failed with message: {}", ex.getMessage());
+            throw ex;
+        }
     }
 
-    public CurrencyExchangeRates getExchangeRatesFallback(String currency, FeignException feignException) {
 
-        log.error("Ooops something went wrong when calling the exchange rate service", feignException);
+    public CurrencyExchangeRates getExchangeRatesCircuitBreakerFallback(String currency, Exception exception) {
+
+        log.info("getExchangeRatesCircuitBreakerFallback");
+        return getExchangeRatesFallback(currency, exception);
+    }
+
+    public CurrencyExchangeRates getExchangeRatesRetryFallback(String currency, Exception exception) {
+
+        log.info("getExchangeRatesRetryFallback");
+        return getExchangeRatesFallback(currency, exception);
+    }
+
+    public CurrencyExchangeRates getExchangeRatesFallback(String currency, Exception exception) {
+
+        log.error("getExchangeRatesFallback applied because of ", exception);
 
         if (latestExchangesRates == null) {
             log.error("No exchange rates found for {}", currency);
             return null;
         }
 
-        log.warn("use the latest received exchange rates received on {}", latestExchangesRates.getDate(), feignException);
+        log.warn("use the latest received exchange rates received on {}", latestExchangesRates.getDate(), exception);
         if (latestExchangesRates.getBase().equals(currency)) {
             // No conversion to perform
             return latestExchangesRates;
